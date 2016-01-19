afbc58b6f1999198c3242d8af0f503c9c9ae45f1
Tim Otten <totten@civicrm.org>
2016-01-19 00:50:23 -0800

    SciptCommand - Cleanup

--- src/Command/ScriptCommand.php
+++ src/Command/ScriptCommand.php
@@ -1,10 +1,8 @@
 <?php
 namespace Civi\Cv\Command;
 
-use Civi\Cv\Util\Filesystem;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
@@ -15,7 +13,7 @@ class ScriptCommand extends BaseCommand {
       ->setName('php-script')
       ->setAliases(array('scr'))
       ->setDescription('Execute a PHP script')
-      ->addArgument('script');
+      ->addArgument('script', InputArgument::REQUIRED);
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
