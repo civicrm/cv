b7cdf5ecef6e960991679d968a2c77508557b13c
Tim Otten <totten@civicrm.org>
2016-01-19 01:27:23 -0800

    BootCommand -- Allow different levels

--- src/Command/BootCommand.php
+++ src/Command/BootCommand.php
@@ -2,6 +2,7 @@
 namespace Civi\Cv\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
@@ -9,15 +10,35 @@ class BootCommand extends BaseCommand {
 
   protected function configure() {
     $this
-      ->setName('php-boot')
-      ->setDescription('Generate PHP bootstrap code');
+      ->setName('php:boot')
+      ->setDescription('Generate PHP bootstrap code')
+      ->addOption('level', NULL, InputOption::VALUE_REQUIRED, 'Bootstrap level (classloader,civi,full)', 'full');
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
     $this->boot($input, $output);
-    $code = \Civi\Cv\Bootstrap::singleton()->generate()
-      . '\CRM_Core_Config::singleton();'
-      . '\CRM_Utils_System::loadBootStrap(array(), FALSE);';
+
+    switch ($input->getOption('level')) {
+      case 'classloader':
+        $code = sprintf('require_once  %s . "/CRM/Core/ClassLoader.php";', var_export(rtrim($GLOBALS["civicrm_root"], '/'), 1))
+          . '\CRM_Core_ClassLoader::singleton()->register();';
+        break;
+
+      case 'civi':
+        $code = \Civi\Cv\Bootstrap::singleton()->generate()
+          . '\CRM_Core_Config::singleton();';
+        break;
+
+      case 'full':
+        $code = \Civi\Cv\Bootstrap::singleton()->generate()
+          . '\CRM_Core_Config::singleton();'
+          . '\CRM_Utils_System::loadBootStrap(array(), FALSE);';
+        break;
+
+      default:
+        break;
+    }
+
     $output->writeln($code);
   }
 
--- tests/Command/BootCommandTest.php
+++ tests/Command/BootCommandTest.php
@@ -9,13 +9,28 @@ class BootlCommandTest extends \Civi\Cv\CivilTestCase {
     parent::setup();
   }
 
-  public function testUrl() {
-    $phpBoot = Process::runOk($this->cv("php-boot"));
+  public function testBootDefault() {
+    $phpBoot = Process::runOk($this->cv("php:boot"));
     $this->assertRegExp(';CIVICRM_SETTINGS_PATH;', $phpBoot->getOutput());
 
-    $helloPhp = escapeshellarg($phpBoot->getOutput() . 'printf("phpr says version is %s\n", CRM_Utils_System::version());');
+    $helloPhp = escapeshellarg($phpBoot->getOutput()
+      . 'printf("count is %s\n", CRM_Core_DAO::singleValueQuery("select count(*) from civicrm_contact"));'
+    );
     $phpRun = Process::runOk(new \Symfony\Component\Process\Process("php -r $helloPhp"));
-    $this->assertRegExp('/^phpr says version is [0-9a-z\.]+$/', $phpRun->getOutput());
+    $this->assertRegExp('/^count is [0-9]+$/', $phpRun->getOutput());
+  }
+
+  public function testBootClassLoader() {
+    $phpBoot = Process::runOk($this->cv("php:boot --level=classloader"));
+    $this->assertRegExp(';ClassLoader;', $phpBoot->getOutput());
+
+    // In the classloader level, config vals like CIVICRM_DSN are not loaded.
+    $helloPhp = escapeshellarg($phpBoot->getOutput()
+      . '$x=array("a"=>defined("CIVICRM_DSN") ? "yup" : "nope");'
+      . 'printf("phpr says %s\n", CRM_Utils_Array::value("a",$x));'
+    );
+    $phpRun = Process::runOk(new \Symfony\Component\Process\Process("php -r $helloPhp"));
+    $this->assertRegExp('/^phpr says nope$/', $phpRun->getOutput());
   }
 
 }
