597a653911dffa49c23f513fa324bcda04493d31
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2016-10-15 22:29:56 +0100

    populator for adding relationships

--- src/Util/Pop/Populator.php
+++ src/Util/Pop/Populator.php
@@ -9,16 +9,16 @@ class Populator {
     $this->optionStore = $optionStore;
   }
 
-  function relationshipFields($entity, $fields){
+  function relationshipFields($entity, &$fields){
 
     // if no relationship type has been specified, get one
     if(!isset($fields['relationship_type_id'])){
-      $fields['relationship_type_id'] = $this->entityStore->getRandom('RelationshipType');
+      $relationshipType = $this->entityStore->getRandom('RelationshipType');
+    }else{
+      $relationshipType = $this->entityStore->getSpecific('RelationshipType', NULL, $fields['relationship_type_id']);
     }
-
-    
-    // get a random contact_a
-
-    // exit;
+    $fields['relationship_type_id']=$relationshipType['id'];
+    $fields['contact_id_a'] = $this->entityStore->getRandom('Contact', array('contact_type' => $relationshipType['contact_type_a']))['id'];
+    $fields['contact_id_b'] = $this->entityStore->getRandom('Contact', array('contact_type' => $relationshipType['contact_type_b']))['id'];
   }
 }
