f78522ab6c89402688425ad2677266b81bb65e1b
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2016-10-15 22:25:49 +0100

    entity store how holds entire entities (not just ids)

--- src/Util/Pop/EntityStore.php
+++ src/Util/Pop/EntityStore.php
@@ -9,13 +9,41 @@ class EntityStore {
 
   private $store = array();
 
-  function getRandom($entity){
-    if(!isset($this->store[$entity])){
-      // 10,000 entities is probably random enough for most people
-      $result = civicrm_api3($entity, 'get', array('return' => array('id'), 'options' => array('limit' => 10000)));
-      $this->entityStore[$entity]=array_keys($result['values']);
+  function getRandom($entity, $filter = NULL){
+    $key = $this->getKey($entity, $filter);
+    return $this->store[$key][$this->getRandomId($entity, $filter)];
+  }
+
+  function getSpecific($entity, $filter = NULL, $id){
+    $key = $this->getKey($entity, $filter);
+    if(!isset($this->store[$key])){
+      $this->init($entity, $filter);
     }
-    return $this->entityStore[$entity][array_rand($this->entityStore[$entity])];
+    return $this->store[$key][$id];
+  }
+
+  function getRandomId($entity, $filter = NULL){
+    $key = $this->getKey($entity, $filter);
+    if(!isset($this->store[$key])){
+      $this->init($entity, $filter);
+    }
+    return array_rand($this->store[$key]);
+  }
+
+  function getKey($entity, $filter){
+    $key = $entity;
+    if(isset($filter)){
+      ksort($filter);
+      $key .= json_encode($filter);
+    }
+    return $key;
+  }
+
+  function init($entity, $filter){
+    $params = array_merge($filter, array('options' => array('limit' => 10000)));
+    $result = civicrm_api3($entity, 'get', $params);
+    // print_r($result);
+    $this->store[$this->getKey($entity, $filter)] = $result['values'];
   }
 
   function add($entity, $id){
