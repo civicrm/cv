aebff1ec6bbf28bb3e0ae6860af6b8f8f5c53a00
Tim Otten <totten@civicrm.org>
2017-02-13 21:23:52 -0800

    PathCommand - Rename "ext:path" to "path". Support -x/-c/-d.

--- src/Command/PathCommand.php
+++ src/Command/PathCommand.php
@@ -21,50 +21,130 @@ class PathCommand extends BaseExtensionCommand {
 
   protected function configure() {
     $this
-      ->setName('ext:path')
+      ->setName('path')
       ->setAliases(array())
-      ->setDescription('Look up an extension path')
+      ->setDescription('Look up the path to a file or directory')
       ->addOption('out', NULL, InputOption::VALUE_REQUIRED, 'Output format (' . implode(',', Encoder::getTabularFormats()) . ')', Encoder::getDefaultFormat('list'))
-      ->addArgument('key-or-name', InputArgument::IS_ARRAY, 'One or more extensions to enable. Identify the extension by full key ("org.example.foobar") or short name ("foobar")')
-      ->setHelp('Look up an extension path
+      ->addOption('columns', NULL, InputOption::VALUE_REQUIRED, 'List of columns to display (comma separated; type, name, value)')
+      ->addOption('ext', 'x', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'An extension name. Identify the extension by full key ("org.example.foobar") or short name ("foobar")')
+      ->addOption('config', 'c', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A config property. (Ex: customFileUploadDir, customPHPPathDir, customTemplateDir, extensionsDir, imageUploadDir, templateCompileDir, uploadDir)')
+      ->addOption('dynamic', 'd', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A dynamic path expression (Ex: "[civicrm.root]/packages")')
+      ->addArgument('file', InputArgument::IS_ARRAY, 'Optionally specify files')
+      ->setHelp('Look up the path to a file or directory
 
-Examples:
-  cv ext:path
-  cv ext:path cividiscount
-  cv ext:path org.civicrm.modules.cividiscount
+Examples (directories):
+  cv path -x cividiscount
+  cv path -x cividiscount -x styleguide -x flexmailer
+  cv path -c templateCompileDir
+  cv path -d \'[civicrm.root]/packages\'
 
-Note:
-  If you don\'t request a specific extension, this command returns the path
-  of the default extension container.
-
-  Beginning circa CiviCRM v4.2+, it has been recommended that extensions
-  include a unique long name ("org.example.foobar") and a unique short
-  name ("foobar"). However, short names are not strongly guaranteed.
+Examples (files):
+  cv path -x cividiscount -x styleguide -x flexmailer info.xml
+  cv path -c uploadDir hello.jpg
+  cv path -d \'[civicrm.root]\' packages/DB.php
 ');
     parent::configureBootOptions();
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
     $this->boot($input, $output);
-    list ($foundKeys, $missingKeys) = $this->parseKeys($input, $output);
 
-    foreach ($missingKeys as $key) {
-      $output->getErrorOutput()->writeln("<comment>Ignoring unrecognized extension \"$key\"</comment>");
-    }
+    $pathResults = array();
+    $returnValue = 0;
 
     $mapper = \CRM_Extension_System::singleton()->getMapper();
-    $results = array();
-    foreach ($foundKeys as $key) {
-      $results[] = array('key' => $key, 'path' => $mapper->keyToBasePath($key));
+    foreach ($input->getOption('ext') as $keyOrName) {
+      if (strpos($keyOrName, '.') === FALSE) {
+        $shortMap = $this->getShortMap();
+        if (isset($shortMap[$keyOrName]) && count($shortMap[$keyOrName]) === 1) {
+          $keyOrName = $shortMap[$keyOrName][0];
+        }
+      }
+
+      try {
+        $pathResults[] = array(
+          'type' => 'ext',
+          'name' => $keyOrName,
+          'value' => \CRM_Utils_File::addTrailingSlash($mapper->keyToBasePath($keyOrName)),
+        );
+      }
+      catch (\CRM_Extension_Exception_MissingException $e) {
+        $output->getErrorOutput()->writeln("<error>Ignoring unrecognized extension \"$keyOrName\"</error>");
+        $returnValue = 1;
+      }
     }
 
-    if (empty($missingKeys) && empty($foundKeys)) {
-      $results[] = array('key' => $key, 'path' => \CRM_Core_Config::singleton()->extensionsDir);
+    foreach ($input->getOption('config') as $configProperty) {
+      $pathResults[] = array(
+        'type' => 'config',
+        'name' => $configProperty,
+        'value' => \CRM_Core_Config::singleton()->{$configProperty},
+      );
     }
 
-    $this->sendTable($input, $output, $results, array('path'));
+    foreach ($input->getOption('dynamic') as $dynExpr) {
+      if (!is_callable(array('Civi', 'paths'))) {
+        $output->getErrorOutput()->writeln("<error>Dynamic path expressions are only available on CiviCRM v4.7+</error>");
+        $returnValue = 1;
+        break;
+      }
 
-    return empty($missingKeys) ? 0 : 1;
+      $fullExpr = preg_match(';^\[[^\]]+\]$;', $dynExpr) ? "$dynExpr/." : $dynExpr;
+
+      $pathResults[] = array(
+        'type' => 'dynamic',
+        'name' => $dynExpr,
+        'value' => \CRM_Utils_File::addTrailingSlash(\Civi::paths()->getPath($fullExpr)),
+      );
+    }
+
+    if (empty($pathResults)) {
+      $output->getErrorOutput()->writeln("<error>No paths found. Must specify -x, -s, or -d. (See also: cv path -h)</error>");
+      return 1;
+    }
+
+    $columns = $this->parseColumns($input, array(
+      'list' => array('value'),
+    ));
+
+    if (!$input->getArgument('file')) {
+      $this->sendTable($input, $output, $pathResults, $columns);
+      return $returnValue;
+    }
+    else {
+      $fileResults = array();
+      foreach ($pathResults as $pathResult) {
+        foreach ($input->getArgument('file') as $file) {
+          $fileResult = $pathResult;
+          $fileResult['value'] .= $file;
+          $fileResults[] = $fileResult;
+        }
+      }
+      $this->sendTable($input, $output, $fileResults, $columns);
+      return $returnValue;
+    }
+  }
+
+  /**
+   * Determine the columns to display.
+   *
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   * @param array $defaultColumns
+   *   Ex: $defaultColumns['table'] = array('name', 'value').
+   * @return array
+   *   Ex: array('*') or array('value').
+   */
+  protected function parseColumns(InputInterface $input, $defaultColumns = array()) {
+    $out = $input->getOption('out');
+    if ($input->getOption('columns')) {
+      return explode(',', $input->getOption('columns'));
+    }
+    elseif (isset($defaultColumns[$out])) {
+      return $defaultColumns[$out];
+    }
+    else {
+      return array('*');
+    }
   }
 
 }
---
+++ tests/Command/PathCommandTest.php
@@ -0,0 +1,84 @@
+<?php
+namespace Civi\Cv\Command;
+
+use Civi\Cv\Util\Process;
+
+class PathCommandTest extends \Civi\Cv\CivilTestCase {
+
+  public function setup() {
+    parent::setup();
+  }
+
+  public function testPaths() {
+    $vars = $this->cvJsonOk('vars:show');
+    $this->assertTrue(is_dir($vars['CIVI_CORE']));
+    $this->assertTrue(file_exists($vars['CIVI_CORE']));
+
+    // Try "cv path -x <extension>".
+    $plain = rtrim($this->cvOk("path -x civicrm"), "\n");
+    $this->assertEquals($vars['CIVI_CORE'], $plain);
+    $json = $this->cvJsonOk("path -x civicrm --out=json");
+    $this->assertEquals('ext', $json[0]['type']);
+    $this->assertEquals('civicrm', $json[0]['name']);
+    $this->assertEquals($vars['CIVI_CORE'], $json[0]['value']);
+  }
+
+  public function testDynamicExprPaths() {
+    $vars = $this->cvJsonOk('vars:show');
+    $this->assertTrue(is_dir($vars['CIVI_CORE']));
+    $this->assertTrue(file_exists($vars['CIVI_CORE']));
+
+    if (version_compare($vars['CIVI_VERSION'], '4.7.0', '<')) {
+      $this->markTestSkipped('"cv path -d" requires v4.7+');
+    }
+
+    $plain = rtrim($this->cvOk("path -d '[civicrm.root]'"), "\n");
+    $this->assertEquals($vars['CIVI_CORE'], $plain);
+
+    $plain = rtrim($this->cvOk("path -d '[civicrm.root]/packages'"), "\n");
+    $this->assertEquals($vars['CIVI_CORE'] . 'packages/', $plain);
+
+    $json = $this->cvJsonOk("path -d '[civicrm.root]/packages' --out=json");
+    $this->assertEquals('dynamic', $json[0]['type']);
+    $this->assertEquals('[civicrm.root]/packages', $json[0]['name']);
+    $this->assertEquals($vars['CIVI_CORE'] . 'packages/', $json[0]['value']);
+  }
+
+  public function testConfigPaths() {
+    $vars = $this->cvJsonOk('vars:show');
+    $this->assertTrue(is_dir($vars['CIVI_CORE']));
+    $this->assertTrue(file_exists($vars['CIVI_CORE']));
+
+    $mandatorySettingNames = array(
+      'customFileUploadDir',
+      'extensionsDir',
+      'imageUploadDir',
+      'templateCompileDir',
+      'uploadDir',
+    );
+    foreach ($mandatorySettingNames as $settingName) {
+      $plain = rtrim($this->cvOk("path -c $settingName"), "\n");
+      $this->assertTrue(file_exists($plain) && is_dir($plain));
+    }
+
+    $optionalSettingNames = array(
+      'customPHPPathDir',
+      'customTemplateDir',
+    );
+    foreach ($optionalSettingNames as $settingName) {
+      $plain = rtrim($this->cvOk("path -c $settingName"), "\n");
+      $this->assertTrue((file_exists($plain) && is_dir($plain)) || empty($plain));
+    }
+  }
+
+  protected function cvOk($cmd) {
+    $p = Process::runOk($this->cv($cmd));
+    return $p->getOutput();
+  }
+
+  protected function cvJsonOk($cmd) {
+    $p = Process::runOk($this->cv($cmd));
+    return json_decode($p->getOutput(), 1);
+  }
+
+}
