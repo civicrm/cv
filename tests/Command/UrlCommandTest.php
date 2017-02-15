7d7126b46c472c715d12e686fcd914ec25b14486
Tim Otten <totten@civicrm.org>
2017-02-15 15:08:06 -0800

    UrlCommand - Add support for -x/-c/-d
    This creates some parallel with the PathCommand.

--- tests/Command/UrlCommandTest.php
+++ tests/Command/UrlCommandTest.php
@@ -3,7 +3,7 @@ namespace Civi\Cv\Command;
 
 use Civi\Cv\Util\Process;
 
-class UrllCommandTest extends \Civi\Cv\CivilTestCase {
+class UrlCommandTest extends \Civi\Cv\CivilTestCase {
 
   public function setup() {
     parent::setup();
@@ -18,6 +18,87 @@ class UrllCommandTest extends \Civi\Cv\CivilTestCase {
     $this->assertRegExp(':angularDebug=1:', $fullUrl);
     $this->assertRegExp(':foo=bar:', $fullUrl);
     $this->assertRegExp(':/mailing/new:', $fullUrl);
+  }
+
+  public function testOutput() {
+    $output = $this->cvFail('url -x. -x. --out=json');
+    $this->assertRegExp(';specify --tabular;', $output);
+
+    $output = $this->cvJsonOk('url -x. -x. --out=json --tabular');
+    $this->assertEquals(2, count($output));
+    $this->assertRegExp(';https?://.*;', $output[0]['value']);
+    $this->assertRegExp(';https?://.*;', $output[1]['value']);
+
+    $output = explode("\n", trim($this->cvOk('url -x. -x. --out=list')));
+    $this->assertEquals(2, count($output));
+    $this->assertRegExp(';https?://.*;', $output[0]);
+    $this->assertRegExp(';https?://.*;', $output[1]);
+
+    $output = $this->cvJsonOk('url -x. --out=json');
+    $this->assertRegExp(';https?://.*;', $output);
+  }
+
+  public function testExtPaths() {
+    $plain = rtrim($this->cvJsonOk("url -x civicrm"), "\n");
+    $this->assertRegExp(';https?://.*/civicrm$;', $plain);
+
+    $plain = rtrim($this->cvJsonOk("url -x civicrm/"), "\n");
+    $this->assertRegExp(';https?://.*/civicrm/$;', $plain);
+
+    $plain = rtrim($this->cvJsonOk("url -x civicrm/packages"), "\n");
+    $this->assertRegExp(';https?://.*/civicrm/packages$;', $plain);
+  }
+
+  public function testDynamicExprPaths() {
+    $vars = $this->cvJsonOk('vars:show');
+    if (version_compare($vars['CIVI_VERSION'], '4.7.0', '<')) {
+      $this->markTestSkipped('"cv path -d" requires v4.7+');
+    }
+
+    $plain = rtrim($this->cvJsonOk("url -d '[civicrm.root]'"), "\n");
+    $this->assertRegExp(';https?://.*/civicrm$;', $plain);
+
+    $plain = rtrim($this->cvJsonOk("url -d '[civicrm.root]/'"), "\n");
+    $this->assertRegExp(';https?://.*/civicrm/$;', $plain);
+
+    $plain = rtrim($this->cvJsonOk("url -d '[civicrm.root]/packages'"), "\n");
+    $this->assertRegExp(';https?://.*/civicrm/packages$;', $plain);
+  }
+
+  public function testConfigPaths() {
+    $vars = $this->cvJsonOk('vars:show');
+    $this->assertTrue(is_dir($vars['CIVI_CORE']));
+    $this->assertTrue(file_exists($vars['CIVI_CORE']));
+
+    $mandatorySettingNames = array(
+      'extensionsURL',
+      'imageUploadURL',
+      'userFrameworkBaseURL',
+      'userFrameworkResourceURL',
+    );
+    foreach ($mandatorySettingNames as $settingName) {
+      $plain = rtrim($this->cvOk("path -c $settingName"), "\n");
+      $this->assertRegExp(';^https?://.*;', $plain, "Check $settingName");
+    }
+
+    $optionalSettingNames = array(
+      'customCSSURL',
+    );
+    foreach ($optionalSettingNames as $settingName) {
+      $plain = rtrim($this->cvOk("path -c $settingName"), "\n");
+      $this->assertRegExp(';^(|https?://.*);', $plain, "Check $settingName");
+    }
+  }
+
+  public function testExtDot() {
+    $this->assertEquals(
+      $this->cvOk('url -c extensionsURL'),
+      $this->cvOk('url -x.')
+    );
+    $this->assertEquals(
+      $this->cvOk('url -c extensionsURL'),
+      $this->cvOk('url -x .')
+    );
   }
 
 }
