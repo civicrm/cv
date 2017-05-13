a3a616f93d2b1610f23564a83aa9dbf03260e030
Andrew Hunt <andrew@aghstrategies.com>
2017-05-13 13:27:00 -0400

    Upgrade get: more complete tests

--- tests/Command/UpgradeGetCommandTest.php
+++ tests/Command/UpgradeGetCommandTest.php
@@ -8,33 +8,81 @@ class UpgradeGetCommandTest extends \Civi\Cv\CivilTestCase {
   }
 
   public function testGetStable() {
-    $p = $this->cv("upgrade:get --stability=stable");
-    $p->run();
-    $data = json_decode($p->getOutput(), 1);
-    $this->assertTrue(isset($data['url']), 'Looking for url in output: ' . $p->getOutput());
-    $this->assertRegExp(';(\.zip|\.gz)$;', $data['url']);
-    $this->assertRegExp('/^([0-9\.\-]|alpha|beta|master|x)+$/', $data['version']);
-    $this->assertRegExp('/^[a-zA-Z0-9\.\-\_]+$/', $data['rev']);
+    $data = $this->upgradeRun('stable');
+    $this->assertRegExp('/\/civicrm-' . $data['version'] . '-(drupal[68]?|joomla|wordpress)+\.(tar\.gz|zip)$/', $data['url']);
+    $this->stableVersionCheck($data);
+  }
+
+  public function testGetStableWordpress() {
+    $data = $this->upgradeRun('stable', 'WordPress');
+    $this->stableVersionCheck($data);
+  }
+
+  public function testGetStableJoomla() {
+    $data = $this->upgradeRun('stable', 'Joomla');
+    $this->stableVersionCheck($data);
+  }
+
+  public function testGetStableDrupal() {
+    $data = $this->upgradeRun('stable', 'Drupal');
+    $this->stableVersionCheck($data);
+  }
+
+  public function testGetStableBackdrop() {
+    $data = $this->upgradeRun('stable', 'Backdrop');
+    $this->stableVersionCheck($data);
   }
 
   public function testGetNightly() {
-    $p = $this->cv("upgrade:get --stability=nightly");
-    $p->run();
-    $data = json_decode($p->getOutput(), 1);
-    $this->assertTrue(isset($data['url']), 'Looking for url in output: ' . $p->getOutput());
-    $this->assertRegExp(';(\.zip|\.gz)$;', $data['url']);
-    $this->assertRegExp('/^([0-9\.\-]|alpha|beta|master|x)+$/', $data['version']);
-    $this->assertRegExp('/^[a-zA-Z0-9\.\-\_]+$/', $data['rev']);
+    $data = $this->upgradeRun('nightly');
+    $revisionId = $this->analyzeRevision($data);
+    $this->assertGreaterThanOrEqual((int) date('Ymdhi', strtotime('-2 days')), (int) $revisionId, 'Nightly revision is well over a day old.');
   }
 
   public function testGetRc() {
-    $p = $this->cv("upgrade:get --stability=rc");
+    $data = $this->upgradeRun('rc');
+    $revisionId = $this->analyzeRevision($data);
+  }
+
+  protected function stableVersionCheck($data) {
+    $this->assertRegExp('/^\d+\.\d+\.\d+$/', $data['version']);
+    $this->assertEquals($data['version'], $data['rev'], 'Stable revision is not the same as version number.');
+  }
+
+  protected function analyzeRevision($data) {
+    $this->assertRegExp('/\d+\.\d+\.\d+$/', $data['version']);
+    $this->assertRegExp('/' . $data['version'] . '-\d+$/', $data['rev'], 'Revision name does not include version number.');
+    $revisionParts = explode('-', $data['rev']);
+    array_unshift($revisionParts, 'civicrm');
+    $revisionId = array_pop($revisionParts);
+    $revisionParts[] = '(drupal[68]?|joomla|wordpress)';
+    $revisionParts[] = $revisionId;
+    $this->assertRegExp('/\/' . implode('-', $revisionParts) . '\.(tar\.gz|zip)$/', $data['url']);
+    $this->assertLessThanOrEqual((int) date('Ymdhi', strtotime('+1 day')), (int) $revisionId, 'Revision is from the future.');
+    return $revisionId;
+  }
+
+  protected function upgradeRun($stability, $cms = NULL) {
+    $cmsString = ($cms) ? " --cms=$cms" : '';
+    $p = $this->cv("upgrade:get --stability=$stability$cmsString");
     $p->run();
     $data = json_decode($p->getOutput(), 1);
     $this->assertTrue(isset($data['url']), 'Looking for url in output: ' . $p->getOutput());
-    $this->assertRegExp(';(\.zip|\.gz)$;', $data['url']);
-    $this->assertRegExp('/^([0-9\.\-]|alpha|beta|master|x)+$/', $data['version']);
-    $this->assertRegExp('/^[a-zA-Z0-9\.\-\_]+$/', $data['rev']);
+    switch ($cms) {
+      case 'WordPress':
+      case 'Joomla':
+        $this->assertRegExp('/-' . strtolower($cms) . '\.zip/', $data['url']);
+        break;
+
+      case 'Drupal':
+      case 'Drupal6':
+      case 'Drupal8':
+      case 'Backdrop':
+        $this->assertRegExp('/-' . strtolower($cms) . '\.tar\.gz/', $data['url']);
+    }
+    $headers = get_headers($data['url']);
+    $this->assertContains('200 OK', $headers[0], 'URL does not have the file to download.');
+    return $data;
   }
 
 }
