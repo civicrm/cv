c46503477cb8b4c2744eaaf2cee81653e7c93f0e
Tim Otten <totten@civicrm.org>
2017-05-17 15:57:43 -0700

    Add command `ang:module:list`

---
+++ src/Command/AngularModuleListCommand.php
@@ -0,0 +1,115 @@
+<?php
+namespace Civi\Cv\Command;
+
+use Civi\Cv\Application;
+use Civi\Cv\Encoder;
+use Civi\Cv\Util\ArrayUtil;
+use Civi\Cv\Util\ExtensionUtil;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class AngularModuleListCommand extends BaseCommand {
+
+  /**
+   * @param string|null $name
+   */
+  public function __construct($name = NULL) {
+    parent::__construct($name);
+  }
+
+  protected function configure() {
+    $this
+      ->setName('ang:module:list')
+      ->setAliases(array())
+      ->setDescription('List Angular modules')
+      ->addOption('columns', NULL, InputOption::VALUE_REQUIRED,
+        'List of columns to display (comma separated)',
+        'name,ext,resources')
+      ->addOption('out', NULL, InputOption::VALUE_REQUIRED,
+        'Output format (' . implode(',', Encoder::getTabularFormats()) . ')',
+        Encoder::getDefaultFormat('table'))
+      ->addArgument('regex', InputArgument::OPTIONAL,
+        'Filter extensions by full key or short name')
+      ->setHelp('List Angular modules
+
+Examples:
+  cv ang:module:list
+  cv ang:module:list \';crmMail;\' --user=admin
+');
+    parent::configureBootOptions();
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $this->boot($input, $output);
+    if (!$input->getOption('user')) {
+      $output->getErrorOutput()->writeln("<comment>For a full list, try passing --user=[username].</comment>");
+    }
+
+    $columns = explode(',', $input->getOption('columns'));
+    $records = $this->sort($this->find($input), $columns);
+
+    $this->sendTable($input, $output, $records, $columns);
+
+    return 0;
+  }
+
+  /**
+   * Find extensions matching the input args.
+   *
+   * @param InputInterface $input
+   * @return array
+   */
+  protected function find($input) {
+    $regex = $input->getArgument('regex');
+    $ang = \Civi::service('angular');
+    $rows = array();
+
+    foreach ($ang->getModules() as $name => $module) {
+      $resources = array();
+      foreach (array('js', 'partials', 'css', 'settings') as $key) {
+        if (!empty($module[$key])) {
+          $resources[] = sprintf("%s(%d)", $key, count($module[$key]));
+        }
+      }
+
+      $rows[] = array(
+        'name' => $name,
+        'ext' => $module['ext'],
+        'resources' => implode(', ', $resources),
+      );
+    }
+
+    $rows = array_filter($rows, function ($row) use ($regex) {
+      if ($regex) {
+        if (!preg_match($regex, $row['ext']) && !preg_match($regex, $row['name'])) {
+          return FALSE;
+        }
+      }
+      return TRUE;
+    });
+
+    return $rows;
+  }
+
+  protected function sort($rows, $orderByColumns) {
+    usort($rows, function ($a, $b) use ($orderByColumns) {
+      foreach ($orderByColumns as $col) {
+        if ($a[$col] < $b[$col]) {
+          return -1;
+        }
+        if ($a[$col] > $b[$col]) {
+          return 1;
+        }
+      }
+
+      return 0;
+    });
+
+    return $rows;
+  }
+
+}
---
+++ tests/Command/AngularModuleListCommandTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Civi\Cv\Command;
+
+use Civi\Cv\Exception\ProcessErrorException;
+use Civi\Cv\Util\Process;
+
+class AngularModuleListCommandTest extends \Civi\Cv\CivilTestCase {
+
+  /**
+   * List extensions using a regular expression.
+   */
+  public function testGetRegex() {
+    $p = Process::runOk($this->cv('ang:module:list'));
+    $this->assertRegexp(';crmUi.*civicrm.*js;', $p->getOutput());
+
+    $p = Process::runOk($this->cv('ang:module:list ";crm;"'));
+    $this->assertRegexp(';crmUi.*civicrm.*js;', $p->getOutput());
+
+    $p = Process::runOk($this->cv('ang:module:list ";foo;"'));
+    $this->assertNotRegexp(';crmUi.*civicrm.*js;', $p->getOutput());
+  }
+
+}
