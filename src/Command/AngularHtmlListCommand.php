eadd74a1ab617957da20d03245cb4fa8d2d03dcc
Tim Otten <totten@civicrm.org>
2017-05-17 15:57:27 -0700

    Add command `ang:html:list`

---
+++ src/Command/AngularHtmlListCommand.php
@@ -0,0 +1,121 @@
+<?php
+namespace Civi\Cv\Command;
+
+use Civi\Cv\Application;
+use Civi\Cv\Encoder;
+use Civi\Cv\Util\ArrayUtil;
+use Civi\Cv\Util\ExtensionUtil;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class AngularHtmlListCommand extends BaseCommand {
+
+  /**
+   * @param string|null $name
+   */
+  public function __construct($name = NULL) {
+    parent::__construct($name);
+  }
+
+  protected function configure() {
+    $this
+      ->setName('ang:html:list')
+      ->setAliases(array())
+      ->setDescription('List Angular HTML files')
+      ->addOption('columns', NULL, InputOption::VALUE_REQUIRED,
+        'List of columns to display (comma separated)',
+        'file')
+      ->addOption('out', NULL, InputOption::VALUE_REQUIRED,
+        'Output format (' . implode(',', Encoder::getTabularFormats()) . ')',
+        Encoder::getDefaultFormat('list'))
+      ->addArgument('filter', InputArgument::OPTIONAL,
+        'Filter by filename. For regex filtering, use semicolon delimiter.')
+      ->setHelp('List Angular HTML files
+
+Examples:
+  cv ang:html:list
+  cv ang:html:list crmUi/*
+  cv ang:html:list \';(tabset|wizard)\\.html;\'
+');
+    parent::configureBootOptions();
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $this->boot($input, $output);
+    if (!$input->getOption('user')) {
+      $output->getErrorOutput()->writeln("<comment>For a full list, try passing --user=[username].</comment>");
+    }
+
+    $columns = explode(',', $input->getOption('columns'));
+    $records = $this->sort($this->find($input), $columns);
+    $this->sendTable($input, $output, $records, $columns);
+    return 0;
+  }
+
+  /**
+   * Find extensions matching the input args.
+   *
+   * @param InputInterface $input
+   * @return array
+   */
+  protected function find($input) {
+    $regex = $input->getArgument('filter') ? $this->createRegex($input->getArgument('filter')) : NULL;
+    $ang = \Civi::service('angular');
+    $rows = array();
+
+    foreach ($ang->getModules() as $name => $module) {
+      $partials = $ang->getPartials($name);
+      foreach ($partials as $file => $html) {
+        $rows[] = array(
+          'file' => preg_replace(';^~/;', '', $file),
+          'module' => $name,
+          'ext' => $module['ext'],
+        );
+      }
+    }
+
+    $rows = array_filter($rows, function ($row) use ($regex) {
+      if ($regex) {
+        if (!preg_match($regex, $row['file'])) {
+          return FALSE;
+        }
+      }
+      return TRUE;
+    });
+
+    return $rows;
+  }
+
+  protected function sort($rows, $orderByColumns) {
+    usort($rows, function ($a, $b) use ($orderByColumns) {
+      foreach ($orderByColumns as $col) {
+        if ($a[$col] < $b[$col]) {
+          return -1;
+        }
+        if ($a[$col] > $b[$col]) {
+          return 1;
+        }
+      }
+
+      return 0;
+    });
+
+    return $rows;
+  }
+
+  protected function createRegex($filterExpr) {
+    if ($filterExpr{0} === ';') {
+      return $filterExpr;
+    }
+    // $filterExpr = preg_replace(';^~/;', '', $filterExpr);
+    $regex = preg_quote($filterExpr, ';');
+    $regex = str_replace('\\*', '[^/]*', $regex);
+    $regex = ";^$regex.*$;";
+    return $regex;
+  }
+
+}
---
+++ tests/Command/AngularHtmlListCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Civi\Cv\Command;
+
+use Civi\Cv\Exception\ProcessErrorException;
+use Civi\Cv\Util\Process;
+
+class AngularHtmlListCommandTest extends \Civi\Cv\CivilTestCase {
+
+  /**
+   * List extensions using a regular expression.
+   */
+  public function testGetRegex() {
+    $p = Process::runOk($this->cv('ang:html:list'));
+    $this->assertRegexp(';crmUi/field.html;', $p->getOutput());
+
+    $p = Process::runOk($this->cv('ang:html:list crmUi')); // matches key
+    $this->assertRegexp(';crmUi/field.html;', $p->getOutput());
+
+    $p = Process::runOk($this->cv('ang:html:list ";field;"')); // matches name
+    $this->assertRegexp(';crmUi/field.html;', $p->getOutput());
+
+    $p = Process::runOk($this->cv('ang:html:list crmAttachment')); // matches name
+    $this->assertNotRegexp(';crmUi/field.html;', $p->getOutput());
+  }
+
+  /**
+   * Get the extension data in an alternate format, eg JSON.
+   */
+  public function testGetJson() {
+    $p = Process::runOk($this->cv('ang:html:list crmUi/field.html --out=json'));
+    $data = json_decode($p->getOutput(), 1);
+    $this->assertEquals(1, count($data));
+    $this->assertEquals('crmUi/field.html', $data[0]['file']);
+  }
+
+}
