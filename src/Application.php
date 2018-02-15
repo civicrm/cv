ce23a060a6897f1b2f3d62595d36d0ec86dde8d4
Tim Otten <totten@civicrm.org>
2018-02-14 18:05:46 -0800

    Application - Temporarily disable most some upgrade commands
    I haven't gone through all these carefully yet, but I'd like to be able to
    merge so that UpgradeDbCommand works.

--- src/Application.php
+++ src/Application.php
@@ -47,11 +47,11 @@ class Application extends \Symfony\Component\Console\Application {
     $commands[] = new \Civi\Cv\Command\FlushCommand();
     $commands[] = new \Civi\Cv\Command\PathCommand();
     $commands[] = new \Civi\Cv\Command\ShowCommand();
-    $commands[] = new \Civi\Cv\Command\UpgradeCommand();
+    // $commands[] = new \Civi\Cv\Command\UpgradeCommand();
     $commands[] = new \Civi\Cv\Command\UpgradeDbCommand();
-    $commands[] = new \Civi\Cv\Command\UpgradeDlCommand();
-    $commands[] = new \Civi\Cv\Command\UpgradeGetCommand();
-    $commands[] = new \Civi\Cv\Command\UpgradeReportCommand();
+    // $commands[] = new \Civi\Cv\Command\UpgradeDlCommand();
+    // $commands[] = new \Civi\Cv\Command\UpgradeGetCommand();
+    // $commands[] = new \Civi\Cv\Command\UpgradeReportCommand();
     $commands[] = new \Civi\Cv\Command\UrlCommand();
     if ($context !== 'repl') {
       $commands[] = new \Civi\Cv\Command\BootCommand();
