ea1879d66f42c789b3562d2da5ad477db088c630
Tim Otten <totten@civicrm.org>
2019-05-28 17:09:22 -0700

    Add OptionCallbackTrait so that configure() can setup better aliases

--- src/Command/BaseCommand.php
+++ src/Command/BaseCommand.php
@@ -2,11 +2,23 @@
 namespace Civi\Cv\Command;
 
 use Civi\Cv\Encoder;
+use Civi\Cv\Util\OptionCallbackTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class BaseCommand extends Command {
+
+  use OptionCallbackTrait;
+
+  /**
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   */
+  protected function initialize(InputInterface $input, OutputInterface $output) {
+    parent::initialize($input, $output);
+    $this->runOptionCallbacks($input, $output);
+  }
 
   protected function assertBooted() {
     if (!$this->isBooted()) {
---
+++ src/Util/OptionCallbackTrait.php
@@ -0,0 +1,61 @@
+<?php
+namespace Civi\Cv\Util;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class OptionCallbackTrait
+ * @package Civi\Cv\Util
+ *
+ * This trait allows you to define an initialization callback for each option.
+ *
+ * Ex:
+ *   $this->addOption('foo', ...)
+ *       ->addOptionCallback('foo', function(InputInterface $input, OutputInterface $output, InputOption $option) { ... })
+ */
+trait OptionCallbackTrait {
+
+  /**
+   * @return \Symfony\Component\Console\Input\InputDefinition
+   *   An InputDefinition instance
+   * @see \Symfony\Component\Console\Command\Command::getDefinition()
+   */
+  abstract public function getDefinition();
+
+  /**
+   * @var array
+   *   Array([string $optionName, callable $callback]).
+   */
+  private $optionCallbacks = [];
+
+  /**
+   * @param string $name
+   *   The name of the option to
+   * @param callable $callback
+   * @return $this
+   */
+  public function addOptionCallback($name, $callback) {
+    $this->optionCallbacks[] = [$name, $callback];
+    return $this;
+  }
+
+  /**
+   * @param \Symfony\Component\Console\Input\InputInterface $input
+   * @param \Symfony\Component\Console\Output\OutputInterface $output
+   * @return $this
+   */
+  protected function runOptionCallbacks(InputInterface $input, OutputInterface $output) {
+    $defn = $this->getDefinition();
+
+    foreach ($this->optionCallbacks as $optionCallbackDefn) {
+      list ($optionName, $callback) = $optionCallbackDefn;
+      if ($defn->hasOption($optionName)) {
+        $callback($input, $output, $defn->getOption($optionName));
+      }
+    }
+
+    return $this;
+  }
+
+}
