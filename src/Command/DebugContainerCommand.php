47321b03d88246cc12cc2b0def02166e12d19721
Tim Otten <totten@civicrm.org>
2021-05-27 16:02:03 -0700

    DebugContainerCommand - Add options for (1) filtering and (2) showing concrete class

--- src/Command/DebugContainerCommand.php
+++ src/Command/DebugContainerCommand.php
@@ -3,7 +3,9 @@ namespace Civi\Cv\Command;
 
 use Civi\Cv\Util\BootTrait;
 use Civi\Cv\Util\StructuredOutputTrait;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DebugContainerCommand extends BaseCommand {
@@ -15,7 +17,8 @@ class DebugContainerCommand extends BaseCommand {
     $this
       ->setName('debug:container')
       ->setDescription('Dump the container configuration')
-      ->addArgument('path')
+      ->addArgument('name', InputArgument::OPTIONAL, 'An service name or regex')
+      ->addOption('concrete', 'C', InputOption::VALUE_NONE, 'Display concrete class names. (This requires activating every matching service.)')
       ->configureOutputOptions(['tabular' => TRUE, 'fallback' => 'table'])
       ->setHelp('
 Dump the container configuration
@@ -35,10 +38,31 @@ Dump the container configuration
       $c->compile();
     }
 
+    $filterPat = $input->getArgument('name');
+    if (empty($filterPat)) {
+      $filter = function () {
+        return TRUE;
+      };
+    }
+    elseif ($filterPat[0] === '/') {
+      $filter = function ($n) use ($filterPat) {
+        return (bool) preg_match($filterPat, $n);
+      };
+    }
+    else {
+      $filter = function ($n) use ($filterPat) {
+        return $n === $filterPat;
+      };
+    }
+
     $rows = array();
     $definitions = $c->getDefinitions();
     ksort($definitions);
     foreach ($definitions as $name => $definition) {
+      if (!$filter($name)) {
+        continue;
+      }
+
       $extras = array();
       foreach (['getFactoryClass', 'getFactoryMethod', 'getFactoryService', 'getFactory'] as $factoryCheck) {
         if (is_callable([$definition, $factoryCheck]) && $definition->$factoryCheck()) {
@@ -51,8 +75,9 @@ Dump the container configuration
       if ($definition->getTags()) {
         $extras[] = sprintf("tags[%s]", count($definition->getTags()));
       }
+      $class = $input->getOption('concrete') ? get_class(\Civi::service($name)) : $definition->getClass();
 
-      $rows[] = array('service' => $name, 'class' => $definition->getClass(), 'extras' => implode(' ', $extras));
+      $rows[] = array('service' => $name, 'class' => $class, 'extras' => implode(' ', $extras));
     }
 
     $this->sendTable($input, $output, $rows, array('service', 'class', 'extras'));
