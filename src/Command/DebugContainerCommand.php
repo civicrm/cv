95164861f8575129f4b10f178bd7c568f88b938b
Tim Otten <totten@civicrm.org>
2022-08-23 00:51:18 -0700

    DebugContainerCommand - Add verbose output mode (to inspect service properties)

--- src/Command/DebugContainerCommand.php
+++ src/Command/DebugContainerCommand.php
@@ -37,50 +37,48 @@ Dump the container configuration
 
     $filterPat = $input->getArgument('name');
     if (empty($filterPat)) {
-      $filter = function ($name, $definition) {
+      $filter = function (Definition $definition, string $name) {
         return TRUE;
       };
     }
     elseif ($filterPat[0] === '/') {
-      $filter = function ($name, $definition) use ($filterPat) {
+      $filter = function (Definition $definition, string $name) use ($filterPat) {
         return (bool) preg_match($filterPat, $name);
       };
     }
     else {
-      $filter = function ($name, $definition) use ($filterPat) {
+      $filter = function (Definition $definition, string $name) use ($filterPat) {
         return $name === $filterPat;
       };
     }
 
     if (!$input->getOption('all')) {
-      $filter = function ($name, Definition $definition) use ($filter) {
-        return $definition->isPublic() && $filter($name, $definition);
+      $filter = function (Definition $definition, string $name) use ($filter) {
+        return $definition->isPublic() && $filter($definition, $name);
       };
     }
 
-    $rows = array();
     $definitions = $c->getDefinitions();
+    $definitions = array_filter($definitions, $filter, ARRAY_FILTER_USE_BOTH);
     ksort($definitions);
-    foreach ($definitions as $name => $definition) {
-      if (!$filter($name, $definition)) {
-        continue;
-      }
 
-      $events = '?';
-      if (class_exists('Civi\Core\Event\EventScanner')) {
-        if ($definition->getTag('event_subscriber') || $definition->getTag('kernel.event_subscriber')) {
-          $events = \Civi\Core\Event\EventScanner::findListeners($definition->getClass());
-        }
-        else {
-          $events = '';
-        }
-      }
+    if ($output->isVerbose()) {
+      $this->showVerboseReport($input, $output, $definitions);
+    }
+    else {
+      $this->showBasicReport($input, $output, $definitions);
+    }
+  }
+
+  public function showBasicReport(InputInterface $input, OutputInterface $output, array $definitions): void {
+    $rows = [];
+
+    foreach ($definitions as $name => $definition) {
+      $events = $this->getEvents($definition);
 
       $extras = array();
-      foreach (['getFactoryClass', 'getFactoryMethod', 'getFactoryService', 'getFactory'] as $factoryCheck) {
-        if (is_callable([$definition, $factoryCheck]) && $definition->$factoryCheck()) {
-          $extras[] = 'factory';
-        }
+      if ($this->getFactory($definition) !== NULL) {
+        $extras[] = 'factory';
       }
       if ($definition->getMethodCalls()) {
         $extras[] = sprintf("calls[%s]", count($definition->getMethodCalls()));
@@ -94,6 +92,29 @@ Dump the container configuration
     }
 
     $this->sendTable($input, $output, $rows, array('service', 'class', 'events', 'extras'));
+  }
+
+  public function showVerboseReport(InputInterface $input, OutputInterface $output, array $definitions): void {
+    foreach ($definitions as $name => $definition) {
+      $row = [];
+      $row[] = ['key' => 'service', 'value' => $name];
+      $row[] = ['key' => 'class', 'value' => $input->getOption('concrete') ? get_class(\Civi::service($name)) : $definition->getClass()];
+      foreach ($definition->getTags() as $tag => $tagData) {
+        $row[] = ['key' => "tag[$tag]", 'value' => $tagData];
+      }
+
+      foreach ($this->getEvents($definition) as $eventName => $eventValue) {
+        $row[] = ['key' => "event[$eventName]", 'value' => $eventValue];
+      }
+      if ($factory = $this->getFactory($definition)) {
+        $row[] = ['key' => 'factory', 'value' => $factory];
+      }
+      foreach ($definition->getMethodCalls() as $n => $call) {
+        $row[] = ['key' => "call[$n]", 'value' => $call];
+      }
+
+      $this->sendTable($input, $output, $row);
+    }
   }
 
   /**
@@ -118,6 +139,40 @@ Dump the container configuration
 
     $container->compile();
     return $container;
+  }
+
+  /**
+   * @param $definition
+   *
+   * @return array|string
+   */
+  protected function getEvents($definition) {
+    if (class_exists('Civi\Core\Event\EventScanner')) {
+      if ($definition->getTag('event_subscriber') || $definition->getTag('kernel.event_subscriber')) {
+        return \Civi\Core\Event\EventScanner::findListeners($definition->getClass());
+      }
+      else {
+        return '';
+      }
+    }
+    return '?';
+  }
+
+  /**
+   * @param \Symfony\Component\DependencyInjection\Definition $definition
+   * @return array|string|null
+   *   A value describing the factory, or NULL if there is none.
+   */
+  protected function getFactory(Definition $definition) {
+    if (is_callable([$definition, 'getFactory'])) {
+      return $definition->getFactory();
+    }
+    foreach (['getFactoryClass', 'getFactoryMethod', 'getFactoryService'] as $factoryCheck) {
+      if (is_callable([$definition, $factoryCheck]) && $definition->$factoryCheck()) {
+        return 'yes';
+      }
+    }
+    return NULL;
   }
 
 }
