21c8bbda898fbdbd410e56503fe651fa0df09ed1
Tim Otten <totten@civicrm.org>
2023-03-12 21:20:47 -0700

    scoper.inc.php - Update exclusion list to newer format

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,17 +2,9 @@
 
 return [
   'prefix' => 'Cvphar',
-  'patchers' => [
-      function (string $filePath, string $prefix, string $content) {
-          // In some cases, `cv` references classes provided by civicrm-core or by the UF. Preserve the original names.
-          $content = preg_replace(';Cvphar\(CRM_|HTML_|DB_|Drupal|JFactory|Guzzle|Civi::);', '$1', $content);
-          $content = preg_replace_callback(';Cvphar\Civi\([A-Za-z0-9_\\]*);', function($m) {
-            if (substr($m[1], 0, 3) === 'Cv\\') return $m[0]; // Civi\Cv is mapped.
-            else return 'Civi\\' . $m[1]; // Nothing else is mapped.
-          }, $content);
-          return $content;
-      },
-  ],
+  'exclude-namespaces' => ['Civi', 'Guzzle', 'Drupal'],
+  'exclude-classes' => ['/^(CRM_|HTML_|DB_)/', 'JFactory', 'Civi', 'Drupal'],
+  'exclude-functions' => ['/^civicrm_api/'],
 
   // Do not generate wrappers/aliases for `civicrm_api()` etc or various CMS-booting functions.
   'expose-global-functions' => false,
