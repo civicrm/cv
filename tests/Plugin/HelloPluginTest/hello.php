ae50ef98bbd376d2ada5f306c07a892ffa3fd070
Tim Otten <totten@civicrm.org>
2024-06-09 17:08:43 -0700

    Incorporate Cv::io (etc) into dev-docs and HelloPluginTest

--- tests/Plugin/HelloPluginTest/hello.php
+++ tests/Plugin/HelloPluginTest/hello.php
@@ -9,15 +9,24 @@ if (empty($CV_PLUGIN['protocol']) || $CV_PLUGIN['protocol'] > 1) {
   die("Expect CV_PLUGIN API v1");
 }
 
+Cv::dispatcher()->addListener('*.app.boot', function ($e) {
+  Cv::io()->writeln("Hello during initial bootstrap!");
+});
+
 Cv::dispatcher()->addListener('cv.app.commands', function ($e) {
   $e['commands'][] = new class extends Command {
 
     protected function configure() {
-      $this->setName('hello')->setDescription('Say a greeting');
+      $this->setName('hello')->setDescription('Say a greeting')->addArgument('name');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-      $output->writeln('Hello there!');
+      if ($input->getArgument('name') !== Cv::input()->getArgument('name')) {
+        throw new \RuntimeException("Argument \"name\" is inconsistent!");
+      }
+      $name = $input->getArgument('name') ?: 'world';
+      $output->writeln("Hello $name via parameter!");
+      Cv::io()->writeln("Hello $name via StyleInterface!");
     }
 
   };
