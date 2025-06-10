civicrm/cv

commit bb30a1a8c6a67ca2807217fb7dc2367d86324a61

PHP 8.4 - Fix warning about E_STRICT
Fix warning:

  Deprecated: Constant E_STRICT is deprecated in ...lib/src/ErrorHandler.php

Reportedly, E_STRICT:
* Is meaningful (from php-src.git POV) in 7.x
* Sopped being meaningful (from php-src.git POV) in 8.x
* Started throwing deprecation warnings in 8.4

It's unclear if there might be significance for contrib in the 8.x era.

lib/src/ErrorHandler.php
@@ -72,23 +72,27 @@ class ErrorHandler {
   }
 
   protected static function getErrorTypes(): array {
-    return [
-      E_ERROR => 'PHP Error',
-      E_WARNING => 'PHP Warning',
-      E_PARSE => 'PHP Parse Error',
-      E_NOTICE => 'PHP Notice',
-      E_CORE_ERROR => 'PHP Core Error',
-      E_CORE_WARNING => 'PHP Core Warning',
-      E_COMPILE_ERROR => 'PHP Compile Error',
-      E_COMPILE_WARNING => 'PHP Compile Warning',
-      E_USER_ERROR => 'PHP User Error',
-      E_USER_WARNING => 'PHP User Warning',
-      E_USER_NOTICE => 'PHP User Notice',
-      E_STRICT => 'PHP Strict Warning',
-      E_RECOVERABLE_ERROR => 'PHP Recoverable Fatal Error',
-      E_DEPRECATED => 'PHP Deprecation',
-      E_USER_DEPRECATED => 'PHP User Deprecation',
-    ];
+    return array_merge(
+      [
+        E_ERROR => 'PHP Error',
+        E_WARNING => 'PHP Warning',
+        E_PARSE => 'PHP Parse Error',
+        E_NOTICE => 'PHP Notice',
+        E_CORE_ERROR => 'PHP Core Error',
+        E_CORE_WARNING => 'PHP Core Warning',
+        E_COMPILE_ERROR => 'PHP Compile Error',
+        E_COMPILE_WARNING => 'PHP Compile Warning',
+        E_USER_ERROR => 'PHP User Error',
+        E_USER_WARNING => 'PHP User Warning',
+        E_USER_NOTICE => 'PHP User Notice',
+        E_RECOVERABLE_ERROR => 'PHP Recoverable Fatal Error',
+        E_DEPRECATED => 'PHP Deprecation',
+        E_USER_DEPRECATED => 'PHP User Deprecation',
+      ],
+      version_compare(phpversion(), '8.4', '<') ? [constant('E_STRICT') => 'PHP Strict Warning'] : []
+      // https://wiki.php.net/rfc/deprecations_php_8_4#remove_e_strict_error_level_and_deprecate_e_strict_constant
+      // In theory, once cv shifts to 8.x only, we can simplify this.
+    );
   }
 
 }

lib/src/Util/BootTrait.php
@@ -330,7 +330,9 @@ trait BootTrait {
     $this->bootLogger($output)->debug('Attempting to set verbose error reporting');
 
     // standard php debug chat settings
-    error_reporting(E_ALL | E_STRICT);
+    error_reporting(E_ALL | (version_compare(phpversion(), '8.4', '<') ? E_STRICT : 0));
+    // https://wiki.php.net/rfc/deprecations_php_8_4#remove_e_strict_error_level_and_deprecate_e_strict_constant
+    // In theory, once cv shifts to 8.x only, we can simplify this.
     ini_set('display_errors', 'stderr');
     ini_set('display_startup_errors', TRUE);
   }

commit c6d3a6a79f7089b89765379412be5cdd135c0d8e

ErrorHandler - Fix extraneous noise about warnings
If a PHP warning is emitted, then we may wind up seeing an oversized message.
The problem arises when trying to decode the warning-level.

Regression from bb30a1a8c6a67ca2807217fb7dc2367d86324a61

lib/src/ErrorHandler.php
@@ -72,27 +72,28 @@ class ErrorHandler {
   }
 
   protected static function getErrorTypes(): array {
-    return array_merge(
-      [
-        E_ERROR => 'PHP Error',
-        E_WARNING => 'PHP Warning',
-        E_PARSE => 'PHP Parse Error',
-        E_NOTICE => 'PHP Notice',
-        E_CORE_ERROR => 'PHP Core Error',
-        E_CORE_WARNING => 'PHP Core Warning',
-        E_COMPILE_ERROR => 'PHP Compile Error',
-        E_COMPILE_WARNING => 'PHP Compile Warning',
-        E_USER_ERROR => 'PHP User Error',
-        E_USER_WARNING => 'PHP User Warning',
-        E_USER_NOTICE => 'PHP User Notice',
-        E_RECOVERABLE_ERROR => 'PHP Recoverable Fatal Error',
-        E_DEPRECATED => 'PHP Deprecation',
-        E_USER_DEPRECATED => 'PHP User Deprecation',
-      ],
-      version_compare(phpversion(), '8.4', '<') ? [constant('E_STRICT') => 'PHP Strict Warning'] : []
+    $const = [
+      E_ERROR => 'PHP Error',
+      E_WARNING => 'PHP Warning',
+      E_PARSE => 'PHP Parse Error',
+      E_NOTICE => 'PHP Notice',
+      E_CORE_ERROR => 'PHP Core Error',
+      E_CORE_WARNING => 'PHP Core Warning',
+      E_COMPILE_ERROR => 'PHP Compile Error',
+      E_COMPILE_WARNING => 'PHP Compile Warning',
+      E_USER_ERROR => 'PHP User Error',
+      E_USER_WARNING => 'PHP User Warning',
+      E_USER_NOTICE => 'PHP User Notice',
+      E_RECOVERABLE_ERROR => 'PHP Recoverable Fatal Error',
+      E_DEPRECATED => 'PHP Deprecation',
+      E_USER_DEPRECATED => 'PHP User Deprecation',
+    ];
+    if (version_compare(phpversion(), '8.4', '<')) {
+      $const[constant('E_STRICT')] = 'PHP Strict Warning';
       // https://wiki.php.net/rfc/deprecations_php_8_4#remove_e_strict_error_level_and_deprecate_e_strict_constant
       // In theory, once cv shifts to 8.x only, we can simplify this.
-    );
+    }
+    return $const;
   }
 
 }